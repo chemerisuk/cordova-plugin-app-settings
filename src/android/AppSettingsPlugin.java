a9ac33bdf536a7587f871588d0b6eb46bda05256
Maksim Chemerisuk <chemerisuk@gmail.com>
2018-06-26 17:20:39 +0300

    initial implementation

--- src/android/AppSettingsPlugin.java
+++ src/android/AppSettingsPlugin.java
@@ -1,6 +1,9 @@
 package by.chemerisuk.cordova.settigns;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import by.chemerisuk.cordova.support.CordovaMethod;
 import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
@@ -14,13 +17,45 @@ import org.json.JSONException;
 public class AppSettingsPlugin extends ReflectiveCordovaPlugin {
     private static final String TAG = "AppSettingsPlugin";
 
+    private SharedPreferences sharedPrefs;
+    private SharedPreferences.Editor sharedPrefsEditor;
+
+    @Override
+    protected void pluginInitialize() {
+        Log.d(TAG, "Starting App Settings plugin");
+
+        Context context = this.cordova.getActivity();
+
+        this.sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        this.sharedPrefsEditor = sharedPrefs.edit();
+    }
+
     @CordovaMethod(ExecutionThread.WORKER)
     protected void getString(String key, CallbackContext callbackContext) {
-
+        if (key == null) {
+            callbackContext.error("Key must not be blank");
+        } else {
+            callbackContext.success(sharedPrefs.getString(key, ""));
+        }
     }
 
     @CordovaMethod(ExecutionThread.WORKER)
     protected void setString(String key, String value, CallbackContext callbackContext) {
+        if (key == null) {
+            callbackContext.error("Key must not be blank");
+        } else {
+            if (value == null) {
+                sharedPrefsEditor.remove(key);
+            } else {
+                sharedPrefsEditor.putString(key, value);
+            }
 
+            boolean success = sharedPrefsEditor.commit();
+            if (success) {
+                callbackContext.success(value);
+            } else {
+                callbackContext.error("Synchronization failed");
+            }
+        }
     }
 }
--- src/ios/AppSettingsPlugin.m
+++ src/ios/AppSettingsPlugin.m
@@ -1,13 +1,55 @@
 #import "AppSettingsPlugin.h"
 
-@implementation AppSettingsPlugin
+@implementation AppSettingsPlugin {
+    NSUserDefaults *defaults;
+}
+
+- (void)pluginInitialize {
+    NSLog(@"Starting Firebase Analytics plugin");
+
+    defaults = [NSUserDefaults standardUserDefaults];
+}
 
 - (void)getString:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        NSString* key = [command.arguments objectAtIndex:0];
 
+        CDVPluginResult* pluginResult = nil;
+        if (key) {
+            NSString* value = [defaults stringForKey:key];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:value];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Key must not be blank"];
+        }
+
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 - (void)setString:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        NSString* key = [command.arguments objectAtIndex:0];
+        NSString* value = [command.arguments objectAtIndex:1];
+        CDVPluginResult* pluginResult = nil;
+        if (key) {
+            if (value) {
+                [defaults setObject:value forKey:key];
+            } else {
+                [defaults removeObjectForKey:value];
+            }
 
+            BOOL success = [defaults synchronize];
+            if (success) {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:value];
+            } else {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Synchronization failed"];
+            }
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Key must not be blank"];
+        }
+
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 @end
